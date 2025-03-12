d98b18c0695aaf87156b2ee5c297ec8719316efb
Hafiz <azan09068@gmail.com>
2025-03-13 00:18:16 +0500

    react-bioler-plate

---
+++ src/store/zustand-store.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+export const useCartStore = create(
+  persist(
+    (set) => ({
+      items: [],
+
+      addToCart: (product) =>
+        set((state) => ({ items: [...state.items, product] })),
+
+      removeFromCart: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.id !== id),
+        })),
+
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: "cart-storage", // Local Storage key
+    }
+  )
+);
